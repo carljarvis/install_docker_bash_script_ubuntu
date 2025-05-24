78ac653be6fed238f143f82740c68ac6dc25a5d6
carljarvis <carl.j.jarvis@gmail.com>
2025-05-24 11:02:04 -0500

    second commit

---
+++ install_docker_script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install docker-ce
+apt-get install docker-ce-cli
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |sudo tee /etc/apt/sources,list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce
+apt-get install docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker run -it --rm -p 8888:8888 -v /home/carljarvis/notebooks/:/mnt/notebooks mimoralea/gdrl:v0.14
+
